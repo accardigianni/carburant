0d38f1bffff4d35412c22ba111504f3a3a4d6303
accardigianni <gianni.accardi@epitech.eu>
2024-03-10 17:08:16 +0100

    selecteur sp98

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 
 function apiCall() {
     return fetch('https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/exports/json')
-        .then(response => response.json());
+        .then(target => target.json());
 }
 
 function printResult() {
-    apiCall()
-        .then(array => {
-            let resultsDiv = document.getElementById('results');
-            array.forEach(value => {
-                let message = `Le prix du SP98 pour ${value.ville} est ${value.sp98_prix}`;
-                let paragraph = document.createElement('p');
-                paragraph.textContent = message;
-                resultsDiv.appendChild(paragraph);
-            });
-        })
-        .catch(error => {
-            console.error(`Une erreur s'est produite :`, error);
-        });
-}
+    let essType = document.getElementById('essSelection').checked;
+    let resultsDiv = document.getElementById('results');
 
-printResult();
+    resultsDiv.innerHTML = '';
+    if (essType) {
+        console.log("coucou");
+        apiCall()
+            .then(array => {
+                array.forEach(value => {
+                    if (value.sp98_prix != null) {
+                        let message = `Le prix du SP98 pour ${value.ville} est ${value.sp98_prix}`;
+                        let paragraph = document.createElement('p');
+                        paragraph.textContent = message;
+                        resultsDiv.appendChild(paragraph);
+                    }
+                });
+            })
+            .catch(error => {
+                console.error(`Une erreur s'est produite :`, error);
+            });
+    }
+}
+document.getElementById('submitButton').addEventListener("click", function () {
+    printResult();
+});
 
 
 
